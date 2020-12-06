8cd5ed76ef004aa785b2f1d7256a65a4f98981af
Lucas Teles <eu@lucasteles.dev>
2020-12-06 12:25:54 -0300

    initial

---
+++ src/Components.fs
@@ -0,0 +1,14 @@
+﻿module Components
+
+open Feliz
+
+[<ReactComponent>]
+let counter() =
+    let (count, setCount) = React.useState(0)
+    Html.div [
+        Html.h1 count
+        Html.button [
+            prop.text "Increment"
+            prop.onClick (fun _ -> setCount(count + 1))
+        ]
+    ]
